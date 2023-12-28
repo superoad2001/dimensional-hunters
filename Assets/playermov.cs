0883237449ccede118ce989baebb0684ce68c1a1
superoad2001 <41108802+superoad2001@users.noreply.github.com>
2023-12-29 00:04:13 +0100

    up

---
+++ Assets/playermov.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class playermov : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
